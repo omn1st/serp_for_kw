993b7ee5358160e7fb8252ccb8d867825b39c23a
Evan Meacham <evan@architek.co>
2019-07-25 18:23:47 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,15 +1,16 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+html = scraperwiki.scrape("http://www.google.com/search?q=web scraping python")
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+root = lxml.html.fromstring(html)
+result = root.cssselect("cite")
+print(result.text)
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
